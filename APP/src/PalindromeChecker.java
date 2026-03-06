cb8e2b1a3334451c3ec80ee1b7b0472e7b418cdf
duttasubhasis2006-max <duttasubhasis2006@gmail.com>
2026-03-06 15:04:53 +0530

    Subhasis ADDED UC11 - Object-Oriented Palindrome Service

--- APP/src/PalindromeChecker.java
+++ APP/src/PalindromeChecker.java
@@ -1,18 +1,27 @@
 import java.util.Scanner;
 
-public class PalindromeCheckerApp {
+public class PalindromeChecker {
 
-    static boolean isPalindrome(String str, int start, int end) {
-        if (start >= end) return true;
-        if (str.charAt(start) != str.charAt(end)) return false;
-        return isPalindrome(str, start + 1, end - 1);
+    static boolean isPalindrome(String str) {
+        str = str.replaceAll("\\s+", "").toLowerCase();
+        int left = 0;
+        int right = str.length() - 1;
+
+        while (left < right) {
+            if (str.charAt(left) != str.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String input = sc.nextLine();
 
-        if (isPalindrome(input, 0, input.length() - 1)) {
+        if (isPalindrome(input)) {
             System.out.println("Palindrome");
         } else {
             System.out.println("Not Palindrome");
