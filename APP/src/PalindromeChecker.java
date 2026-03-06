cf7d16614d2f6529657961bb6f0b67a8ad2e8e81
duttasubhasis2006-max <duttasubhasis2006@gmail.com>
2026-03-06 15:10:30 +0530

    Subhasis ADDED  UC12 - Strategy

--- APP/src/PalindromeChecker.java
+++ APP/src/PalindromeChecker.java
@@ -1,27 +1,59 @@
-import java.util.Scanner;
+import java.util.*;
+
+interface PalindromeStrategy {
+    boolean isPalindrome(String str);
+}
+
+class StackStrategy implements PalindromeStrategy {
+    public boolean isPalindrome(String str) {
+        Stack<Character> stack = new Stack<>();
+        for (char c : str.toCharArray()) {
+            stack.push(c);
+        }
+        for (char c : str.toCharArray()) {
+            if (c != stack.pop()) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
+class DequeStrategy implements PalindromeStrategy {
+    public boolean isPalindrome(String str) {
+        Deque<Character> deque = new ArrayDeque<>();
+        for (char c : str.toCharArray()) {
+            deque.add(c);
+        }
+        while (deque.size() > 1) {
+            if (deque.pollFirst() != deque.pollLast()) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
 
 public class PalindromeChecker {
 
-    static boolean isPalindrome(String str) {
-        str = str.replaceAll("\\s+", "").toLowerCase();
-        int left = 0;
-        int right = str.length() - 1;
+    static PalindromeStrategy strategy;
 
-        while (left < right) {
-            if (str.charAt(left) != str.charAt(right)) {
-                return false;
-            }
-            left++;
-            right--;
-        }
-        return true;
+    public static void setStrategy(PalindromeStrategy strategy) {
+        PalindromeChecker.strategy = strategy;
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String input = sc.nextLine();
+        int choice = sc.nextInt();
 
-        if (isPalindrome(input)) {
+        if (choice == 1) {
+            setStrategy(new StackStrategy());
+        } else {
+            setStrategy(new DequeStrategy());
+        }
+
+        if (strategy.isPalindrome(input)) {
             System.out.println("Palindrome");
         } else {
             System.out.println("Not Palindrome");
